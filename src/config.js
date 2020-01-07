b56c384c2774347d9e1683b5b63a642e84a3e19a
윤남기 <skarlsladlek@naver.com>
2020-01-07 16:51:15 +0900

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "notes-apps-skarl"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://hjanjunu4a.execute-api.us-west-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_RKbH8MqEE",
+      APP_CLIENT_ID: "27n7pt7q0p0q0o66enupm89ivo",
+      IDENTITY_POOL_ID: "us-west-2:d7c0f0d2-9a05-4f22-ad14-ccf24ae68348"
+    }
+  };
